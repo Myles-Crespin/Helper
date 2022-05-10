0f051b8edf5b49ec9f12d916e8b89b8968c91c9e
ia4252 <95661852+ia4252@users.noreply.github.com>
2022-05-10 08:57:41 -0400

    Update Hub.js

--- Hub.js
+++ Hub.js
@@ -4,16 +4,8 @@ function setup() {
   buttons = [];
   let texts = [1, 2, 3, 4, 5, 6];
   let urls = [1, 2, 3, 4, 5, 6];
-  let colors = [
-    color(255, 255, 255),
-    color(255, 255, 255),
-    color(255, 255, 255),
-    color(255, 255, 255),
-    color(255, 255, 255),
-    color(255, 255, 255),
-  ];
   for (let i = 0; i < 6; i++) {
-    buttons[i] = new Button(texts, urls, colors, i);
+    buttons[i] = new Button(texts, urls, i);
     buttons[i].display();
   }
 }
@@ -26,22 +18,29 @@ function draw() {
   text("8th Grade Helper", width / 2, height / 4);
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].redirect();
+    if (
+      mouseX < buttons[i].button.x + buttons[i].button.width &&
+      mouseX > buttons[i].button.x &&
+      mouseY > buttons[i].button.y &&
+      mouseY < buttons[i].button.y + buttons[i].button.height
+    ) {
+      buttons[i].mouseIsOver();
+    }
   }
 }
 
 class Button {
-  constructor(word, url, color, number) {
+  constructor(word, url, number) {
     this.word = word;
     this.url = url;
-    this.color = color;
     this.width = width / 4 - width / 12;
     this.height = height / 12;
     this.x = width / 4 + (number % 2) * (this.width + width / 6);
     this.y = height / 2 + int(number / 2) * (this.height + this.height / 2);
+    this.button = createButton("test");
   }
   display() {
-    this.button = createButton("test");
-    this.button.style("background-color", this.color);
+    this.button.style("background-color", 255);
     this.button.style("font-size", 30 + "px");
     this.button.position(this.x, this.y);
     this.button.size(this.width, this.height);
@@ -50,5 +49,8 @@ class Button {
     this.button.mousePressed(function buttonpress() {
       window.location.replace(this.url);
     });
+  }
+  mouseIsOver() {
+    this.button.style("background-color", 200);
   }
 }
