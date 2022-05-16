1b099baad5afeb7f93a71f435d0745e5bf904363
ia4252 <95661852+ia4252@users.noreply.github.com>
2022-05-16 16:54:47 -0400

    Update Hub.js

--- Hub.js
+++ Hub.js
@@ -7,11 +7,13 @@ function setup() {
   for (let i = 0; i < 6; i++) {
     buttons[i] = new Button(texts, urls, i);
     buttons[i].display();
+    myButton = new Button1((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/")
   }
 }
 
 function draw() {
   background(0);
+  myButton.doStuff()
   fill(0, 255, 0);
   textSize(50);
   textAlign(CENTER, CENTER);
@@ -54,3 +56,25 @@ class Button {
     this.button.style("background-color", 200);
   }
 }
+class Button1{
+  constructor(x,y,w,h,t,u){
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+    this.u = u
+    this.button = createButton(t)
+    this.button.position(x,y)
+    this.button.size(w,h) 
+  this.button.style("background-color", 255);
+    this.button.style("font-size", height / 26 + "px");  }
+doStuff(){
+  if (
+      mouseX < myButton.button.x + myButton.button.width &&
+      mouseX > myButton.button.x &&
+      mouseY > myButton.button.y &&
+      mouseY < myButton.button.y + myButton.button.height
+    ) {myButton.button.style("background-color", 200)}
+  myButton.button.mousePressed(function back(){window.location.replace(myButton.u);
+                               })
+}}
