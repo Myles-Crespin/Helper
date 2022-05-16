02977db3fb8298d14e429d02ba3e43dbc89e40e7
ia4252 <95661852+ia4252@users.noreply.github.com>
2022-05-16 13:16:28 -0400

    Update BackButton.js

--- BackButton.js
+++ BackButton.js
@@ -1,1 +1,32 @@
 
+let myButton;
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  myButton = new Button((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/")
+}
+
+function draw() {
+  background(220);
+  if (
+      mouseX < myButton.button.x + myButton.button.width &&
+      mouseX > myButton.button.x &&
+      mouseY > myButton.button.y &&
+      mouseY < myButton.button.y + myButton.button.height
+    ) {myButton.button.style("background-color", 200)}
+  myButton.button.mousePressed(function back(){window.location.replace(myButton.u);
+                               })
+  
+}
+class Button{
+  constructor(x,y,w,h,t,u){
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+    this.u = u
+    this.button = createButton(t)
+    this.button.position(x,y)
+    this.button.size(w,h) 
+  this.button.style("background-color", 255);
+    this.button.style("font-size", height / 26 + "px");  }
+}
