2b7b7f00c1d94a1456524d2ab1187ad40d334b4c
ia4252 <95661852+ia4252@users.noreply.github.com>
2022-05-20 11:36:06 -0400

    Update WASD-Game.js

--- WASD-Game.js
+++ WASD-Game.js
@@ -1,15 +1,15 @@
 let x1, y1, w1, l1, x2, y2, w2, l2, direction, directions, right1, right2, speed1, speed2, img, img2, img3, YN
-
 function setup() {
-  createCanvas(400, 400);
-  x1 = 10;
-  y1 = 10;
-  w1 = 50;
-  l1 = 50;
-  x2 = 200;
-  y2 = 200;
-  w2 = 50;
-  l2 = 50;
+  createCanvas(window.innerWidth, window.innerHeight);
+  
+  x1 = 10
+  y1 = 10
+  w1 = 50
+  l1 = 50
+  x2 = width/2
+  y2 = height/2
+  w2 = 50
+  l2 = 50
   speed1 = 1
   speed2 = 10
   YN = true
@@ -18,7 +18,8 @@ function setup() {
   img3 = loadImage('download (1).jpeg')
   directions = ['right', 'left', 'up', 'down'];
   direction = random(directions);
-   myButton = new Button1((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/")
+  myButton = new Button1((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/")
+
 }
 
 function draw() {
@@ -43,7 +44,7 @@ function draw() {
       y1 = y1 + speed1
     }
     direction = random(directions)
-
+myButton.doStuff()
     if (direction == 'right') {
       x2 = x2 + speed2
     } else if (direction == 'left') {
@@ -53,7 +54,6 @@ function draw() {
     } else if (direction == 'down') {
       y2 = y2 + speed2
     }
-    myButton.doStuff()
 
     right1 = x1 + w1
     right2 = x2 + w2
@@ -62,10 +62,10 @@ function draw() {
     if (right1 >= x2 && right2 >= x1 && bottom1 >= y2 && bottom2 >= y1) {
       win()
     }
-    if (right1 >= 400 || x1 <= 0 || bottom1 >= 400 || y1 <= 0) {
+    if (right1 >= width || x1 <= 0 || bottom1 >= height || y1 <= 0) {
       lose2()
     }
-    if (right2 >= 400 || x2 <= 0 || bottom2 >= 400 || y2 <= 0) {
+    if (right2 >= width || x2 <= 0 || bottom2 >= height || y2 <= 0) {
       lose1()
     }
 
@@ -77,22 +77,22 @@ function draw() {
 function win() {
   textSize(50)
   fill(70, 250, 0)
-  image(img, 0, 0, 400, 430)
-  text('YOU WON!!!', height / 2 - 130, width / 2 + 10)
+  image(img, 0, 0,width, height+height/10)
+  text('YOU WON!!!', width / 2 - width/3, height / 2 + height/40)
   textSize(20)
-  text('You Collided with the enemy and they blew up!', height / 2 - 195, width - 100)
-  text('Press "S" to restart', height / 2 - 60, width / 2 + 180)
+  text('You Collided with the enemy and they blew up!', (width -400)/ 2, height - height/4)
+  text('Press "S" to restart', width / 2 - width/7, height-height/20)
   YN = false
 }
 
 function lose1() {
   textSize(50)
   fill(70, 250, 0)
-  image(img2, 0, 0, 400, 430)
-  text('YOU LOST     :(', height / 2 - 180, width / 2 + 10)
+  image(img2, 0, 0, width, height+height/10)
+  text('YOU LOST     :(', width / 2 - width/3, height / 2 + height/40)
   textSize(18)
-  text('You left the gate open and the enemy escaped!', height / 2 - 190, width - 100)
-  text('Press "S" to restart', height / 2 - 60, width / 2 + 180)
+  text('You left the gate open and the enemy escaped!', (width -400)/ 2, height - height/4)
+  text('Press "S" to restart', width / 2 - width/7, height -height/20)
   YN = false
 
 }
@@ -100,11 +100,11 @@ function lose1() {
 function lose2() {
   textSize(50)
   fill(70, 250, 0)
-  image(img3, 0, 0, 400, 450)
-  text('YOU LOST     :(', height / 2 - 180, width / 2 + 10)
+  image(img3, 0, 0, width, height+height/8)
+  text('YOU LOST     :(', width / 2 - width/3, height / 2 + height/40)
   textSize(18)
-  text('You left the prison and quit your job!', height / 2 - 140, width / 2 + 50)
-  text('Press "S" to restart', height / 2 - 60, width / 2 + 180)
+  text('You left the prison and quit your job!', (width -400)/ 2, height / 1.6)
+  text('Press "S" to restart', width / 2.8, height - height/20)
   YN = false
 
 
