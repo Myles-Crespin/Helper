de084542a4af1f9259fc1e2c507a50e8ef3982d9
ia4252 <95661852+ia4252@users.noreply.github.com>
2022-05-24 08:30:24 -0400

    Update BackButton.js

--- BackButton.js
+++ BackButton.js
@@ -1,7 +1,7 @@
-let myButton
-
-class Button1{
-  constructor(x,y,w,h,t,u){
+//this program creates the back button; it is called from other files so it is only a class.
+let myButton;
+class Button1{//button class
+  constructor(x,y,w,h,t,u){//defining vairables and changing some button settings
     this.x = x
     this.y = y
     this.w = w
@@ -12,7 +12,7 @@ class Button1{
     this.button.size(w,h) 
   this.button.style("background-color", 255);
     this.button.style("font-size", height / 26 + "px");  }
-doStuff(){
+doStuff(){//this is called in the draw function of most programs; it does stuff when the mouse is over the button and when the button is pressed.
   if (
       mouseX < myButton.button.x + myButton.button.width &&
       mouseX > myButton.button.x &&
