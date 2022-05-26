40f3e81fd9a16e7423681361b3a5d94ecbcb718c
mc3412 <95657676+Myles-Crespin@users.noreply.github.com>
2022-05-26 13:27:26 -0400

    Update Therapist.js

--- Therapist.js
+++ Therapist.js
@@ -1,55 +1,60 @@
 let myAnswerClass;
 let box; //creating a submission box
-let button2;
-let y;
-let b2;
-let b3;
+let button2; //creating a second button
+let b2; //no button
+let b3; //yes button
 
 //creates the buttons position
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight);//makes canvas window height and width
   myAnswerClass = new AnswerClass(); //creates class
   box = createInput(); //creating a submission box
   box.position(75, 100); //defines the box position
   button = createButton("Submit"); //creates a submit button
   button.position(box.x + box.width, box.y); //creates the buttons position
-  b2 = false;
-  b3 = false;
-  button.mousePressed(gAnswer);
-   myButton = new Button1((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/")
+  b2 = false; //no button is false
+  b3 = false; //yes button is false
+  button.mousePressed(gAnswer); //when the first button is pressed call function gAnswer
+   myButton = new Button1((width*3)/4,0,width/4,height/10,"Back to Hub","https://ia4252.github.io/Helper/") //creates class
 }
 
 function draw() {
   background(220);
-myButton.doStuff()
+myButton.doStuff() //allows the button to work
   myAnswerClass.display(); //display function
   if (myAnswerClass.answerNumber > 1) {
     myAnswerClass.newQuestion(); //if the mouse has been pressed show other question
   }
   if (myAnswerClass.answerNumber > 2) {
+    //when the two previous questions have been answered
     myAnswerClass.finalAnswer(); //show final answer after final click
-    button2 = createButton("No");
-    button2.position(300, 310);
-    button2.mousePressed(trueOrFalse1);
-    button3 = createButton("Yes");
-    button3.position(250, 310);
-    button3.mousePressed(trueOrFalse2);
+    button2 = createButton("No"); //gives the no button a value
+    button2.position(300, 310); //sets position
+    button2.mousePressed(trueOrFalse1); //when pressed calls trueOrFalse function
+    button3 = createButton("Yes"); //gives the yes button a value
+    button3.position(250, 310); //sets position
+    button3.mousePressed(trueOrFalse2); //when pressed calles the other trueOrFalse function
 
-    text("Are you satisfied with this answer?", 150, 300);
+    text("Are you satisfied with this answer?", 150, 300); //displays text
   }
   if (b2) {
+    //shows text
     text("Well, too bad.", 300, 350);
   } else if (b3) {
+    //shows text
     text("Enjoy your newly learned wisdom.", 300, 350);
   }
 }
 function trueOrFalse1() {
+  //makes b2 true so that the if statement is true
   b2 = true;
 }
 function trueOrFalse2() {
+  //makes b3 true so that the if statement is true
   b3 = true;
 }
 function gAnswer() {
+  //clears answer box
   myAnswerClass.update(box.value());
   box.value("");
 }
