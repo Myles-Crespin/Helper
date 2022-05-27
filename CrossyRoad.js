1e31ec50e5f3e0de110d3f0197bb0c4b12350ed2
mc3412 <95657676+Myles-Crespin@users.noreply.github.com>
2022-05-27 18:11:43 -0400

    Update CrossyRoad.js

--- CrossyRoad.js
+++ CrossyRoad.js
@@ -11,7 +11,7 @@ function setup() {
     cars[i] = new Car(
       i * 20,
       random(30, window.innerHeight - 10),
-      random(1, 8)
+      random(1, 15)
     );
     //cars array equals class
   }
